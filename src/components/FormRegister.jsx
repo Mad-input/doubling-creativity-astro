aab18fc4c1c6479d708b805dc6f6451ef3af0db8
Miguel Contreras <macc07112005c80@gmail.com>
2024-08-14 00:32:40 -0500

    Add form verification

--- src/components/FormRegister.jsx
+++ src/components/FormRegister.jsx
@@ -1,21 +1,112 @@
+import { useForm } from 'react-hook-form'
+import TagError from './TagError.jsx'
+
 export default function FormRegister() {
+  const {
+    handleSubmit,
+    register,
+    watch,
+    formState: {
+      errors
+    }
+  } = useForm()
+
+  const optionsUsername = {
+    required: {
+      value: true,
+      message: "name is required"
+    },
+    minLength: {
+      value: 3,
+      message: "minimum trhee characters"
+    },
+    maxLength: {
+      value: 20,
+      message: "maximum twenty characters"
+    }
+  }
+
+  const optionsEmail = {
+    required: {
+      value: true,
+      message: "Email is required",
+    },
+    pattern: {
+      value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+      message: 'invalid email'
+    }
+  }
+
+  const optionsPassword = {
+    required: {
+      value: true,
+      message: "password is required"
+    },
+    minLength: {
+      value: 6,
+      message: "must be a minimum of six"
+    }
+  }
+
+  const optionsConfirmPassword = {
+    required: {
+      value: true,
+      message: "password is required"
+    },
+    validate: (value) => value === watch('password') ? true : 'passwords do not match'
+  }
+
+
+  const onSubmit = handleSubmit((values) => {
+    console.log(values);
+  })
+
   return (
-    <form id="form">
+    <form id="form" onSubmit={onSubmit}>
       <h2 className="title-form">Sign Up</h2>
       <div className="logo">
-        <img src="/img/logo-icon.svg" alt="logo" />
+        <a href="/"><img src="/img/logo-icon.svg" alt="logo" /></a>
       </div>
       <div className="containt-input">
-        <input type="text" placeholder="" />
+        <input
+          className={`${errors.username ? 'error' : 'ok'}`}
+          type="text"
+          placeholder=""
+          autoFocus
+          {...register("username", optionsUsername)}
+          autoComplete='off' />
         <label>Username</label>
+        {errors.username && <TagError text={errors.username.message} />}
       </div>
       <div className="containt-input">
-        <input type="email" placeholder="" />
+        <input
+          className={`${errors.email ? 'error' : 'ok'}`}
+          type="email"
+          placeholder=""
+          {...register("email", optionsEmail)}
+          autoComplete='off' />
         <label>Email</label>
+        {errors.email && <TagError text={errors.email.message} />}
       </div>
       <div className="containt-input">
-        <input type="password" placeholder="" />
+        <input
+          className={`${errors.password ? 'error' : 'ok'}`}
+          type="password"
+          placeholder=""
+          {...register("password", optionsPassword)}
+          autoComplete='off' />
         <label>Password</label>
+        {errors.password && <TagError text={errors.password.message} />}
+      </div>
+      <div className="containt-input">
+        <input
+          className={`${errors.confirmPassword ? 'error' : 'ok'}`}
+          type="password"
+          placeholder=""
+          {...register("confirmPassword", optionsConfirmPassword)}
+          autoComplete='off' />
+        <label>Confirm Password</label>
+        {errors.confirmPassword && <TagError text={errors.confirmPassword.message} />}
       </div>
       <button type="submit" className="btn-submit">Register</button>
     </form>
---
+++ src/components/TagError.jsx
@@ -0,0 +1,5 @@
+export default function TagError({ text }) {
+  return (
+    <span className="tag-error">{text}</span>
+  )
+}
