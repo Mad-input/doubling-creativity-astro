81bd7082c80913a94411340e3eaee2667860b923
Miguel Contreras <macc07112005c80@gmail.com>
2024-08-29 00:04:47 -0500

    Add endpoint

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,10 +1,23 @@
 import axios from 'axios'
 import { API } from '../../config'
 
-export const registerUser = async (values) => await axios.post(`${API}/register`,values)
+const registerUser = async (values) => await axios.post(`${API}/register`,values)
 
-export const loginUser = async (values) => await axios.post(`${API}/login`, values, {withCredentials: true})
+const loginUser = async (values) => await axios.post(`${API}/login`, values, {withCredentials: true})
 
-export const getProfile = async () => await axios.get(`${API}/profile`,{withCredentials: true})
+const getProfile = async () => await axios.get(`${API}/profile`,{withCredentials: true})
 
-export const logoutUser = async () => await axios.post(`${API}/logout`,{},{withCredentials: true})
+const logoutUser = async () => await axios.post(`${API}/logout`,{},{withCredentials: true})
+
+const createDetail = async (values) => await axios.post(`${API}/details`,values, {withCredentials: true})
+
+const getAllDetails = async () => await axios.get(`${API}/details`, {withCredentials: true})
+
+export {
+  registerUser,
+  loginUser,
+  getProfile,
+  logoutUser,
+  createDetail,
+  getAllDetails
+}
